8a2380c0fa45e8e6634d5b5b366c8e4335428ee9
anfelole <anfelole11082001@gmail.com>
2022-08-10 16:48:11 -0500

    crud bd

--- dashboard/bd/conexion.php
+++ dashboard/bd/conexion.php
@@ -1,10 +1,10 @@
 <?php 
 class Conexion{	  
     public static function Conectar() {        
-        define('servidor', 'localhost');
-        define('nombre_bd', 'crud_2019');
-        define('usuario', 'root');
-        define('password', '');					        
+        define('servidor', 'sql470.main-hosting.eu');
+        define('nombre_bd', 'u349577050_andres');
+        define('usuario', 'u349577050_anfelole');
+        define('password', '@nFeLoLe11');					        
         $opciones = array(PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES utf8');			
         try{
             $conexion = new PDO("mysql:host=".servidor."; dbname=".nombre_bd, usuario, password, $opciones);			
